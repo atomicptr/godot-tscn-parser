ea86d1b35fc15ce9a748a72e9b70d31a53007f20
Christopher Kaster <me@atomicptr.de>
2021-02-14 19:48:01 +0100

    add test tag, remove example from tests/coverage reports

---
+++ pkg/tscn/tscn_test.go
@@ -0,0 +1,24 @@
+package tscn
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestParseScene(t *testing.T) {
+	content := `[gd_scene]
+[ext_resource path="res://Test.tscn" type="PackedScene" id=1]
+[node name="Root" type="Node2D"]
+[node name="Child" type="Node2D" parent="."]
+position = Vector2(13, 37)`
+	scene, err := ParseScene(strings.NewReader(content))
+	assert.NoError(t, err)
+	assert.Len(t, scene.ExtResources, 1)
+}
+
+func TestParseSceneWithInvalidScene(t *testing.T) {
+	content := `[gd_scene`
+	_, err := ParseScene(strings.NewReader(content))
+	assert.Error(t, err)
+}
